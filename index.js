7ddc2248bfd1c9c82f301f31e5d85f677b479bd3
iammahir2020 <1810659@iub.edu.bd>
2022-04-30 05:27:21 +0600

    single item delete route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,12 @@ async function run() {
       const query = { creater: userEmail };
       const cursor = itemsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.delete("/item", async (req, res) => {
+      const itemId = req.query.id;
+      const query = { _id: ObjectId(itemId) };
+      const result = await itemsCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
