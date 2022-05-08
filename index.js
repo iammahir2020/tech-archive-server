ab1939852abd369965fc1f6fc933f64b001b2528
iammahir2020 <1810659@iub.edu.bd>
2022-05-08 14:10:16 +0600

    GET route for getting add reviews added

--- index.js
+++ index.js
@@ -106,6 +106,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/review", async (req, res) => {
       const review = req.body;
       const result = await reviewsCollection.insertOne(review);
